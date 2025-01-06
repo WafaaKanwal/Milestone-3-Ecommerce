51d36ed03206261d3099d063b1392376de1126b6
Wafa Kanwal <160244541+WafaaKanwal@users.noreply.github.com>
2025-01-07 03:44:29 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignore linting errors during build
   },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+  },
 };
 
 export default nextConfig;
